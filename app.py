4d8dd91b731e54e9c0af946bb49968d3d4af5706
Avneet thakur <athakur443@gmail.com>
2020-10-29 11:56:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+import sklearn
+import pickle
+import pandas as pd
+
+classify = pickle.load(open("classifier.pkl", "rb"))
+cv = pickle.load(open("BOW.pkl","rb"))
+
+app = Flask(__name__)
+
+@app.route('/')
+@cross_origin()
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    if request.method == 'POST':
+    	message = request.form['message']
+    	data = [message]
+    	vect = cv.transform(data).toarray()
+    	my_prediction = classify.predict(vect)
+    	return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=5000)
